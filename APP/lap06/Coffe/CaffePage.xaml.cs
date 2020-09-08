670ce7f2311efa1ca14ab81c2f3a80c546f64238
tranvantuan132001 <54611397+tranvantuan132001@users.noreply.github.com>
2020-09-08 16:49:20 +0700

    lap 5,6

---
+++ APP/lap06/Coffe/CaffePage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Display.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Coffe
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CaffePage : Page
+    {
+        public CaffePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MenuFlyoutItem_Click(object sender, RoutedEventArgs e)
+        {
+            //var selected = new(MenuFlyoutItem)sender;
+            //_roast
+        }
+        private void Sweetener_Click(object sender,RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _sweetener = selected.Text;
+            DisplayResult();
+
+        }
+        private void Cream_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+           _cream = selected.Text;
+            DisplayResult();
+
+        }
+        private void Roast_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _roast = selected.Text;
+            DisplayResult();
+
+        }
+        private void displayResult()
+        {
+            if(_roast == "none" || string.IsNullOrEmpty(_roast))
+            {
+                ResultTextBlock.Text = "None";
+                return;
+
+            }
+            ResultTextBlock.Text = _roast;
+            if (_cream != "None" && !string.IsNullOrEmpty(_cream))
+                ResultTextBlock.Text += "+" + _Cream;
+
+            if (_sweetener != "None" && !string.IsNullOrEmpty(_sweetener))
+                ResultTextBlock.Text += "+" + _sweetener;
+        }
+
+    }
+}
---
+++ APP/lap5b2/lap5b2/024.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace lap5b2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class _024 : Page
+    {
+        public _024()
+        {
+            this.InitializeComponent();
+
+            MyFram.Navigate(typeof(Financial));
+            TittleTextBlock.Text = "Financial";
+
+            BackButton.Visibility = Visibility.Collapsed;
+            Financial.IsSelected = true;
+        }
+        private void HumbergerButton_Click(object sender,RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = MySplitView.IsPaneOpen;
+
+        }
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyFram.CanGoBack)
+            {
+                MyFram.GoBack();
+                Financial.IsSelected = true;
+            }
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(Financial.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Collapsed;
+                MyFram.Navigate(typeof(Financial));
+
+            }    
+            else if(Food.IsSelected){
+                BackButton.Visibility = Visibility.Visible;
+                MyFram.Navigate(typeof(Food));
+                TittleTextBlock.Text = "Food";
+
+
+            }
+
+        }
+    }
+}
