6e3d9b467d7c1031ee14a5b53ea3098f0b5aa445
tranvantuan132001 <54611397+tranvantuan132001@users.noreply.github.com>
2020-10-03 11:14:01 +0700

    1

---
+++ wedform/lab01/WingtipToys/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Data.Entity;
+using WingtipToys.Models;
+
+namespace WingtipToys
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //            Initialize the product database.
+            Database.SetInitializer(new ProductDatabaseInitializer());
+            // ctr + k +r xem đc sử dụng ở chỗ nào 
+        }
+
+    }
+}
---
+++ wedform/lab01/WingtipToys/Models/ProductDatabaseInitializer.cs.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WingtipToys.Models
+{
+    public class ProductDatabaseInitializer : DropCreateDatabaseAlways<ProductContext>
+    {
+        protected override void Seed(ProductContext context)
+        {
+            GetCategories().ForEach(c => context.Categories.Add(c));
+            GetProducts().ForEach(p => context.Products.Add(p));
+        }
+        private static List<Category> GetCategories()
+        {
+            var categories = new List<Category>
+            {
+                new Category
+                {
+                CategoryID = 1,
+                CategoryName = "Cars"
+                },
+                 new Category
+                {
+                CategoryID = 2,
+                CategoryName = "adc"
+                },
+                  new Category
+                {
+                CategoryID = 3,
+                CategoryName = "abc"
+                },
+                   new Category
+                {
+                CategoryID = 4,
+                CategoryName = "sdc"
+                },
+                    new Category
+                {
+                CategoryID = 5,
+                CategoryName = "the rocks"
+                },
+
+            };
+            return categories;
+        }
+
+        private static List<Product> GetProducts()
+        {
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    ProductID = 1,
+                    ProductName = "Convertible Car",Description = "This convertible car is fast! The engine is  powered by a neutrino based battery (not included)." +
+
+                    "Power it up and let it go!",
+                    ImagePath="carconvert.png",
+                    UnitPrice = 22.50,
+                    CategoryID = 1
+                },
+                new Product
+                {
+                    ProductID = 2,
+                    ProductName = "Convertible Car",Description = "This convertible car is fast! The engine is  powered by a neutrino based battery (not included)." +
+
+                    "Power it up and let it go!",
+                    ImagePath="carconvert.png",
+                    UnitPrice = 19.50,
+                    CategoryID = 1
+                },
+                new Product
+                {
+                    ProductID = 3,
+                    ProductName = "Convertible Car",Description = "This convertible car is fast! The engine is  powered by a neutrino based battery (not included)." +
+
+                    "Power it up and let it go!",
+                    ImagePath="carconvert.png",
+                    UnitPrice = 17.90,
+                    CategoryID = 1
+                },
+                new Product
+                {
+                    ProductID = 4,
+                    ProductName = "Convertible Car",Description = "This convertible car is fast! The engine is  powered by a neutrino based battery (not included)." +
+
+                    "Power it up and let it go!",
+                    ImagePath="carconvert.png",
+                    UnitPrice = 21.90,
+                    CategoryID = 1
+                },
+                new Product
+                {
+                    ProductID = 5,
+                    ProductName = "Convertible Car",Description = "This convertible car is fast! The engine is  powered by a neutrino based battery (not included)." +
+
+                    "Power it up and let it go!",
+                    ImagePath="carconvert.png",
+                    UnitPrice = 34.98,
+                    CategoryID = 1
+                },
+
+
+            };
+            return products;
+        }
+
+
+        }
+       
+
+}
