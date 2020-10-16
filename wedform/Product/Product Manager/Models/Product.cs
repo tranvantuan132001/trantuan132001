2b1262fd6534cd89018248bba12a50d361607757
tranvantuan132001 <54611397+tranvantuan132001@users.noreply.github.com>
2020-10-16 11:00:23 +0700

    .
    .

---
+++ wedform/Product/Product Manager/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Product_Manager.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    }
+}
